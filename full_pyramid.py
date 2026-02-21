9c8f38ba4c3f75d46e0d2e5a4140a9e7f7a2117f
malekz <malekzaid402@gmail.com>
2026-02-21 16:43:57 +0530

    success

---
+++ full_pyramid.py
@@ -0,0 +1,8 @@
+# full_pyramid.py
+
+rows = 5
+
+for i in range(rows, 0, -1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
---
+++ right_half_pyramid.py
@@ -0,0 +1,8 @@
+# right_half_pyramid.py
+
+rows = 6
+
+for i in range(1, rows + 1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
