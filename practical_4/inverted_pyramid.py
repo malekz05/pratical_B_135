ccd94af40cb20d0957e79cbef7042aaeb8cfca9d
malekz05 <malekzaid402@gmail.com>
2026-03-24 19:37:00 +0530

    submit
    Added a new pattern to print inverted right half pyramid with letters.

--- practical_4/inverted_pyramid.py
+++ practical_4/inverted_pyramid.py
@@ -6,3 +6,12 @@ for i in range(rows, 0, -1):
     for j in range(i):
         print("*", end=" ")
     print()
+
+rows = 5
+
+for i in range(rows, 0, -1):
+    ch = ord('A')
+    for j in range(i):
+        print(chr(ch), end=" ")
+        ch += 1
+    print()
